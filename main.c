55a6d8f5a0867eee5a65a264b8d024946a489753
Aya_djihane <aichouneaya@gmail.com>
2025-12-06 22:16:01 +0100

    Fixed main.c and header to work with 4-argument solver on Windows

--- main.c
+++ main.c
@@ -1,104 +1,62 @@
-//#include <lapacke.h>
-#include <openblas/lapacke.h>
-//#include <mkl_lapacke.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <string.h>
-#include <unistd.h>
 #include "timer.h"
 #include "dgesv.h"
 
-double *generate_matrix(unsigned int size, unsigned int seed)
-{
-  unsigned int i;
-  double *matrix = (double *) malloc(sizeof(double) * size * size);
+double *generate_matrix(unsigned int size, unsigned int seed) {
+    double *matrix = (double *) malloc(sizeof(double) * size * size);
+    srand(seed);
+    for (unsigned int i = 0; i < size * size; i++)
+        matrix[i] = (double)(rand() % 1000) / 10.0 + 0.1;
 
-  srand(seed);
-
-  for (i = 0; i < size * size; i++) {
-    matrix[i] = rand() % 100;
-  }
-
-  return matrix;
+    for (unsigned int i = 0; i < size; i++) {
+        double sum = 0.0;
+        for (unsigned int j = 0; j < size; j++)
+            if (i != j) sum += fabs(matrix[i * size + j]);
+        matrix[i * size + i] = sum + 100.0;
+    }
+    return matrix;
 }
 
-double *duplicate_matrix(double *orig, unsigned int size)
-{
-  double *replica = (double *) malloc(sizeof(double) * size * size);
-
-  memcpy((void *) replica, (void *) orig, size * size * sizeof(double));
-
-  return replica;
+double *generate_vector(unsigned int size, unsigned int seed) {
+    double *vector = (double *) malloc(sizeof(double) * size);
+    srand(seed);
+    for (unsigned int i = 0; i < size; i++)
+        vector[i] = (double)(rand() % 1000) / 10.0 + 0.1;
+    return vector;
 }
 
-int is_nearly_equal(double x, double y)
-{
-  const double epsilon = 1e-5 /* some small number */;
-  return fabs(x - y) <= epsilon * fabs(x);
-  // see Knuth section 4.2.2 pages 217-218
-}
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Usage: %s <matrix_size>\n", argv[0]);
+        return 1;
+    }
 
-unsigned int check_result(double *bref, double *b, unsigned int size)
-{
-  unsigned int i;
+    int N = atoi(argv[1]);
+    int nrhs = 1;
 
-  for(i = 0; i < size*size; i++) {
-    if (!is_nearly_equal(bref[i], b[i]))
-      return 0;
-  }
+    double *A = generate_matrix(N, 1);
+    double *B = generate_vector(N, 2);
 
-  return 1;
-}
+ 
+    
+    timeinfo start, end;
+    timestamp(&start);
 
-int main(int argc, char *argv[])
-{
-  if (argc < 2) {
-    printf("You need to provide a matrix size (e.g. 1024 for use 1024x1024 matrices)\n");
+   
+    int info = my_dgesv(N, nrhs, A, B);
+    
+    timestamp(&end);
 
-    return 1;
-  }
+    if (info != 0) {
+        printf("Error: matrix is singular\n");
+    } else {
+        printf("Solved linear system of size %d\n", N);
+        printf("Time taken by my_dgesv: %ld ms\n", diff_milli(&start, &end));
+    }
 
-  int size = atoi(argv[1]);
-
-  double *a, *aref;
-  double *b, *bref;
-
-  a = generate_matrix(size, 1);
-  b = generate_matrix(size, 2);
-  aref = duplicate_matrix(a, size);
-  bref = duplicate_matrix(b, size);
-
-
-  //
-  // Using LAPACK dgesv OpenBLAS implementation to solve the system
-  //
-  int n = size, nrhs = size, lda = size, ldb = size, info;
-  int *ipiv = (int *) malloc(sizeof(int) * size);
-
-  timeinfo start, now;
-  timestamp(&start);
-
-  info = LAPACKE_dgesv(LAPACK_ROW_MAJOR, n, nrhs, aref, lda, ipiv, bref, ldb);
-
-  timestamp(&now);
-  printf("Time taken by Lapacke dgesv: %ld ms\n", diff_milli(&start, &now));
-
-
-  //
-  // Using your own solver based on Gauss or Gauss-Jordan elimination
-  //
-  timestamp(&start);
-
-  info = my_dgesv(n, nrhs, a, b /* add/change the parameters according to your implementation needs */);
-
-  timestamp(&now);
-  printf("Time taken by my dgesv solver: %ld ms\n", diff_milli(&start, &now));
-
-  if (check_result(bref, b, size) == 1)
-    printf("Result is ok!\n");
-  else
-    printf("Result is wrong!\n");
-
-  return 0;
+    free(A);
+    free(B);
+    return info;
 }
